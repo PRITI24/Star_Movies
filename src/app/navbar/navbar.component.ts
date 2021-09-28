28e5a6560f3403a989ed50c7ab08529579876681
Priti Jha <42322432+PRITI24@users.noreply.github.com>
2021-09-28 13:07:59 +0530

    Show movies

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,Input,Output, EventEmitter} from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { map, switchMap, debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { MovieService } from 'src/app/movie.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  @Input() movies:FormControl = new FormControl()
+  movies$: Observable<any[]>=new Observable;
+  constructor(private movieService: MovieService) { }
 
   ngOnInit(): void {
+    this.movies = new FormControl();
+
+    this.movies$ = this.movies.valueChanges
+      .pipe(
+        debounceTime(500),
+        distinctUntilChanged(),
+        switchMap(
+          searchString => this.movieService.getMovieBySearchTerm(searchString)
+        ),
+        map((res:any) => res.Search)
+        
+      );
+      console.log(this.movies$)
   }
 
 }
